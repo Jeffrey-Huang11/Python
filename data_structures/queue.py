2e250807f37780b4497ff2f7e0a836af7b4f0fd7
Jeffrey Huang <71650487+Jeffrey-Huang11@users.noreply.github.com>
2023-01-04 13:37:06 -0500

    added stack and node

--- data_structures/queue.py
+++ data_structures/queue.py
@@ -1,16 +1,4 @@
-class Node:
-    def __init__(self, value, next_node=None):
-        self.value = value
-        self.next_node = next_node
-    
-    def set_next_node(self, next_node):
-        self.next_node = next_node
-    
-    def get_next_node(self):
-        return self.next_node
-  
-    def get_value(self):
-        return self.value
+from node import Node
 
 class Queue:
     def __init__(self, max_size=None):
